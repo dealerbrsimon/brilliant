aa28b03367984b7e0737cc9d1f6eeae963c2f27c
dealerbrsimon <53241567+dealerbrsimon@users.noreply.github.com>
2019-08-06 12:28:02 -0400

    Create ex.py

---
+++ ex.py
@@ -0,0 +1,14 @@
+'''
+Exemple of a message to encode, the chart to encrypt it and the resulting message. The passage is in lower case to simplify visualization and it is from HP Lovecraft's. 
+'''
+
+
+original_message = 'that is not dead which can eternal lie and with strange aeons even death may die'
+
+crypted_message = 'wkdw lv qrw ghdg zklfk fdq hwhuqdo olh dqg zlwk vwudqjh dhrqv hyhq ghdwk pdb glh'
+
+ceasar_cypher = {
+
+	'a': 'd', 'b': 'e', 'c': 'f', 'd': 'g', 'e': 'h', 'f': 'i', 'g': 'j', 'h': 'k', 'i': 'l', 'j': 'm', 'k': 'n', 'l': 'o', 'm': 'p', 'n':'q', 'o': 'r', 'p': 's', 'q': 't', 'r': 'u', 's': 'v', 't': 'w', 'u': 'x', 'v': 'y', 'w': 'z', 'x': 'a', 'y': 'b', 'z': 'c'
+
+}
